18bccc7d6d2805decbd69b39979762214e1e2d0c
JarJarBings12 <JarJarBingse12@gmail.com>
2014-11-08 09:44:29 +0100

    Add the working Version of the Render Window Class

--- HelpBookWinBasic/src/ch/JarJarBings12/helpbookwin/basic/configuration/configuration.java
+++ HelpBookWinBasic/src/ch/JarJarBings12/helpbookwin/basic/configuration/configuration.java
@@ -4,7 +4,6 @@ import ch.JarJarBings12.helpbookwin.basic.files.filelist;
 
 public class configuration {
 	public void createConfig() {
-		
 		filelist.co.addDefault("helpbook.windows.remove.RemoveOnlyOutOfCache", false);
 		filelist.co.addDefault("helpbook.windows.backup.timed.enabled", true);
 		filelist.co.addDefault("helpbook.windows.backup.timed.interval", 600);
--- HelpBookWinBasic/src/ch/JarJarBings12/helpbookwin/basic/render/WindowRender.java
+++ HelpBookWinBasic/src/ch/JarJarBings12/helpbookwin/basic/render/WindowRender.java
@@ -1,7 +1,5 @@
 package ch.JarJarBings12.helpbookwin.basic.render;
 
-import net.minecraft.server.v1_7_R3.Item;
-
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -12,31 +10,71 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import ch.JarJarBings12.helpbookwin.basic.files.filelist;
 import ch.JarJarBings12.helpbookwin.basic.objects.JObjects;
+import ch.JarJarBings12.helpbookwin.basic.objects.WinBasic;
 import ch.JarJarBings12.helpbookwin.basic.windows.Objects.windowsObj;
 
 public class WindowRender {
-	public void renderWindow(String Window, Player pl) {
-//		Inventory tempinv = pl.getServer().createInventory(null, JObjects.getWindowByName(name).getLines()*9, JObjects.getWindowByName(name).getDisplayName());
-		windowsObj w = getWinObject(Window);
-		Inventory TempInv = pl.getServer().createInventory(null, w.getLines()*9, w.getDisplayName());
+	
+	public WindowRender(WinBasic inCore) {
+		WinBasic.inWindow = inCore;
+	}
+	
+	public void renderWindow(windowsObj Window, Player pl) {
+		
+		/* w = Window */
+		windowsObj w = Window;
+		
+		/* x = lines*9 */
+		int x = w.getLines()*9;
+		
+		/* Create a Virtual Inventory x = lines w.getDisplayName() get the Name of the Window */
+		Inventory TempInv = pl.getServer().createInventory(null, x, w.getDisplayName());
+		
+		/* Create the Default Item Stack */
 		ItemStack leer = new ItemStack(Material.AIR);
-		for(int i = 0; i < w.getLines()*9; i++) {
-			if(filelist.s.getBoolean("windows.window."+w.getSystemName()+".ObjList.object"+i+".ENABLED") == true) {
-				if(filelist.s.getString("windows.window."+w.getSystemName()+".ObjList.object"+i+".TYPE") ==  "BUTTON") {
+		
+		/* Go from 0 to max 54 and list all Item Object's into the Window */
+		for(int i = 0; i < x; i++) {
+			
+			/* Create a boolean for it is be use for check is this Slot enabled */
+			boolean enabled = filelist.s.getBoolean("windows.window."+w.getSystemName()+".ObjList.object"+i+".ENABLED");
+			
+			/* Check is Slot Enabled */
+			if(enabled == true) {
+				
+				/* Get the Type of the Slot out of the Configuration */
+				String TYPE = filelist.s.getString("windows.window."+w.getSystemName()+".ObjList.object"+i+".TYPE");
+				
+				/* If the Slot is a Button */
+				if(TYPE.equals("BUTTON")) {
+					
+					String mat = filelist.s.getString("windows.window."+w.getSystemName()+".ObjList.object"+i+".MATERIAL");
+					Material m = Material.getMaterial(mat);
+					
+					/* If the Material is Air skip the Meta options */
+					if(m == Material.AIR) {
+						TempInv.setItem(i, leer);
+					} else {
+						ItemStack item = new ItemStack(m);
+						ItemMeta meta = (ItemMeta)item.getItemMeta();
+						meta.setDisplayName(filelist.s.getString("windows.window."+w.getSystemName()+".ObjList.object"+i+".DISPLAYNAME"));
+						meta.setDisplayName(filelist.s.getString("windows.window."+w.getSystemName()+".ObjList.object"+i+".LORE"));
+						item.setItemMeta(meta);
+						TempInv.setItem(i, item);	
+					}
+				
+					/* If the Slot Type is a Label */
+				} else if (TYPE ==  "LABEL") {				
 					Material m = Material.getMaterial(filelist.s.getString("windows.window."+w.getSystemName()+".ObjList.object"+i+".MATERIAL"));
 					ItemStack item = new ItemStack(m);
 					ItemMeta meta = (ItemMeta)item.getItemMeta();
 					meta.setDisplayName(filelist.s.getString("windows.window."+w.getSystemName()+".ObjList.object"+i+".DISPLAYNAME"));
 					meta.setDisplayName(filelist.s.getString("windows.window."+w.getSystemName()+".ObjList.object"+i+".LORE"));
 					item.setItemMeta(meta);
-				} else if (filelist.s.getString("windows.window."+w.getSystemName()+".ObjList.object"+i+".TYPE") ==  "LABEL") {
-					Material m = Material.getMaterial(filelist.s.getString("windows.window."+w.getSystemName()+".ObjList.object"+i+".MATERIAL"));
-					ItemStack item = new ItemStack(m);
-					ItemMeta meta = (ItemMeta)item.getItemMeta();
-					meta.setDisplayName(filelist.s.getString("windows.window."+w.getSystemName()+".ObjList.object"+i+".DISPLAYNAME"));
-					meta.setDisplayName(filelist.s.getString("windows.window."+w.getSystemName()+".ObjList.object"+i+".LORE"));
-					item.setItemMeta(meta);
-				} else if (filelist.s.getString("windows.window."+w.getSystemName()+".ObjList.object"+i+".TYPE") ==  "BOOK") {
+					TempInv.setItem(i, item);
+				
+					/* If the Slot Type is a Book */
+				} else if (TYPE==  "BOOK") {
 					ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
 					BookMeta meta =(BookMeta) book.getItemMeta();
 					meta.setAuthor(filelist.s.getString("windows.window."+w.getSystemName()+".ObjList.object"+i+".AUTHOR"));
@@ -44,27 +82,24 @@ public class WindowRender {
 					meta.setLore(filelist.s.getStringList("windows.window."+w.getSystemName()+".ObjList.object"+i+".LORE"));
 					book.setItemMeta(meta);
 					TempInv.setItem(i, book);
+				
+					/* If the Slot Type is Unknown */
 				} else {
 					TempInv.setItem(i, leer);
 				}
+			
+				/* If the Slot is Disabled */
 			} else {
 				TempInv.setItem(i, leer);
 			}
+			
+			/* Check have the Window a Enable Sound or not. */
 			if((filelist.s.getBoolean("windows.window."+Window+".OPENSOUND")) != false ) {
 				pl.playSound(pl.getLocation(), Sound.HORSE_ARMOR, 1, 1);
 			}
+			
+			/* Open the TempInv and Open it. */
 			pl.openInventory(TempInv);
 		}
 	}
-	
-	private windowsObj getWinObject(String Name) {
-		for (windowsObj w : JObjects.windows) {
-			if(w.getSystemName() == Name) {
-				return w;
-			}
-		}
-		return null;
-	}
-	
-	
 }
